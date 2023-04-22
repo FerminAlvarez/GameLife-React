31631610433b226031640332c9eea89d69f34114
Fermin Alvarez <feeralvarez013@gmail.com>
2023-04-22 02:18:14 -0300

    Changed alt img

--- src/components/BoardPreviewHome.jsx
+++ src/components/BoardPreviewHome.jsx
@@ -1,7 +1,7 @@
 export default function BoardPreviewHome({ avatar, name, title, description, gif }) {
     return (
         <div className="card w-72 max-h-72 bg-base-100 shadow-xl image-full overflow-hidden ">
-            <img src={gif} alt="Shoes"/>
+            <img src={gif} alt="board preview"/>
             <div className="card-body">
                 <label class="avatar">
                     <div class="w-10 rounded-full">
