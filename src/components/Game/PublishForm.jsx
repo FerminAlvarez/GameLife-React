5ff610c145eadab421d6141677878e7c2b2bf5db
Fermin Alvarez <feeralvarez013@gmail.com>
2023-04-27 14:26:00 -0300

    Fixed navigate after publish

--- src/components/Game/PublishForm.jsx
+++ src/components/Game/PublishForm.jsx
@@ -1,15 +1,13 @@
 
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import { saveGrid } from '../../services/BoardService';
 import { useNavigate } from 'react-router';
 export default function PublishForm({ initGrid }) {
     let [title, setTitle] = useState()
     let [description, setDescription] = useState()
-    const navigate = useNavigate()
     return (
         <form onSubmit={() => {
             saveGrid(initGrid, title, description);
-            navigate("/explore");
         }}>
             <div className='flex flex-col items-center'>
                 <input type="text" minLength="5" placeholder="Title" className="input input-bordered w-full max-w-xs m-5" required onChange={e => setTitle(e.target.value)} />
